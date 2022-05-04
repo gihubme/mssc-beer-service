a9dffc8c9f1817d31ff5505f52e943b655c7780e
gihubme <n.karakin@qmul.ac.uk>
2022-05-04 12:23:30 +0200

    beer ctrl. Closes #3

---
+++ src/main/java/org/nnn4eu/mssc/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,37 @@
+package org.nnn4eu.mssc.msscbeerservice.web.controller;
+
+import org.nnn4eu.mssc.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("id") UUID id) {
+//        todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<BeerDto> handlePost(@RequestBody BeerDto dto) {
+//        todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBeerById(@PathVariable("id") UUID id) {
+//        todo impl
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateBeerById(@PathVariable("id") UUID id) {
+//        todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}

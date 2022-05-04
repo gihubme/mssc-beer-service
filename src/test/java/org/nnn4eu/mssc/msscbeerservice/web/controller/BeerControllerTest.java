b06fe983433d61e26ee76ed583906c65c8ed514e
gihubme <n.karakin@qmul.ac.uk>
2022-05-04 12:51:23 +0200

    beer ctrl test. Closes #4

---
+++ src/test/java/org/nnn4eu/mssc/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,65 @@
+package org.nnn4eu.mssc.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.nnn4eu.mssc.msscbeerservice.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @DisplayName("Get /beerById ok")
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("POST /beer created")
+    @Test
+    void saveNewBeer() throws Exception {
+        BeerDto dto = BeerDto.builder().build();
+        String dtoJson = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJson))
+                .andExpect(status().isCreated());
+    }
+
+    @DisplayName("DELETE /beerById noContent")
+    @Test
+    void deleteBeerById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/beer/" + UUID.randomUUID().toString()))
+                .andExpect(status().isNoContent());
+//        then(service).should().deleteById(any());
+    }
+
+    @DisplayName("PUT /beerById noContent")
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDto dto = BeerDto.builder().build();
+        String dtoJson = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJson))
+                .andExpect(status().isNoContent());
+    }
+}

9c50405c1481a8b8224b57ef4c5fd844f4e4c7a6
gihubme <n.karakin@qmul.ac.uk>
2022-07-01 09:33:37 +0200

    dummy

--- src/main/java/org/nnn4eu/mssc/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/org/nnn4eu/mssc/msscbeerservice/MsscBeerServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MsscBeerServiceApplication.class, args);
+        
     }
 
 }
